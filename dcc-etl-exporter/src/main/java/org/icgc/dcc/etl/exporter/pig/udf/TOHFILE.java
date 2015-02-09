484204d06d55b9941e10f1a00dc69d6b22e9316c
Sepehr Mavedati <sepehr.mavedati@oicr.on.ca>
2015-02-09 13:03:49 -0500

    Updated exporter's dependency on downloader.

--- dcc-etl-exporter/src/main/java/org/icgc/dcc/etl/exporter/pig/udf/TOHFILE.java
+++ dcc-etl-exporter/src/main/java/org/icgc/dcc/etl/exporter/pig/udf/TOHFILE.java
@@ -30,10 +30,12 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.hbase.io.compress.Compression;
+import org.apache.hadoop.hbase.io.compress.Compression.Algorithm;
 import org.apache.hadoop.hbase.io.hfile.CacheConfig;
-import org.apache.hadoop.hbase.io.hfile.Compression;
 import org.apache.hadoop.hbase.io.hfile.HFile;
 import org.apache.hadoop.hbase.io.hfile.HFile.Writer;
+import org.apache.hadoop.hbase.io.hfile.HFileContextBuilder;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.pig.EvalFunc;
 import org.apache.pig.data.BagFactory;
@@ -54,168 +56,177 @@ import com.google.common.base.Preconditions;
 
 public class TOHFILE extends EvalFunc<DataBag> {
 
-  private static int BLOCKSIZE = 5 * 1048576;
-  private static String COMPRESSION = Compression.Algorithm.SNAPPY.getName();
-  private String[] headers;
-  private final String destDir;
-  private final String tablename;
-  private Writer writer;
-  private ArchiveMetaManager metaManager;
-  private boolean hasDoneHeader = false;
-  private int previousId;
-  private final String metaTablename;
-  private final boolean isPerGroupOutput;
+	private static int BLOCKSIZE = 5 * 1048576;
+	private static Algorithm COMPRESSION = Compression.Algorithm.SNAPPY;
+	private String[] headers;
+	private final String destDir;
+	private final String tablename;
+	private Writer writer;
+	private ArchiveMetaManager metaManager;
+	private boolean hasDoneHeader = false;
+	private int previousId;
+	private final String metaTablename;
+	private final boolean isPerGroupOutput;
 
-  public TOHFILE(String dataType, String releaseName, String destDir,
-      String isPerDonorOutputFile) throws IOException {
-    super();
-    this.destDir = destDir;
-    this.tablename = dataType;
-    this.isPerGroupOutput = Boolean.valueOf(isPerDonorOutputFile);
-    if (releaseName == null || releaseName.trim().isEmpty()) {
-      this.metaTablename = ArchiverConstant.META_TABLE_NAME;
-    } else {
-      this.metaTablename = SchemaUtil.getMetaTableName(releaseName);
-    }
-  }
+	public TOHFILE(String dataType, String releaseName, String destDir,
+			String isPerDonorOutputFile) throws IOException {
+		super();
+		this.destDir = destDir;
+		this.tablename = dataType;
+		this.isPerGroupOutput = Boolean.valueOf(isPerDonorOutputFile);
+		if (releaseName == null || releaseName.trim().isEmpty()) {
+			this.metaTablename = ArchiverConstant.META_TABLE_NAME;
+		} else {
+			this.metaTablename = SchemaUtil.getMetaTableName(releaseName);
+		}
+	}
 
-  public TOHFILE(String dataType, String releaseName, String destDir)
-      throws IOException {
-    this(dataType, releaseName, destDir, "false");
-  }
+	public TOHFILE(String dataType, String releaseName, String destDir)
+			throws IOException {
+		this(dataType, releaseName, destDir, "false");
+	}
 
-  private Writer createWriter(String donorId, UDFContext context)
-      throws IOException {
-    Configuration conf = UDFContext.getUDFContext().getJobConf();
-    getLogger().info("Initializing a new HFile Writer...");
-    FileSystem fs = FileSystem.get(conf);
-    Path destPath = new Path(destDir, Bytes.toString(DATA_CONTENT_FAMILY));
-    return HFile.getWriterFactory(conf, new CacheConfig(conf))
-        .createWriter(fs, new Path(destPath, donorId), BLOCKSIZE,
-            COMPRESSION, KeyValue.KEY_COMPARATOR);
-  }
+	private Writer createWriter(String donorId, UDFContext context)
+			throws IOException {
+		Configuration conf = UDFContext.getUDFContext().getJobConf();
+		getLogger().info("Initializing a new HFile Writer...");
+		FileSystem fs = FileSystem.get(conf);
+		Path destPath = new Path(destDir, Bytes.toString(DATA_CONTENT_FAMILY));
+		return HFile
+				.getWriterFactory(conf, new CacheConfig(conf))
+				.withPath(fs, new Path(destPath, donorId))
+				.withComparator(KeyValue.COMPARATOR)
+				.withFileContext(
+						new HFileContextBuilder().withBlockSize(BLOCKSIZE)
+								.withCompression(COMPRESSION).build()).create();
+	}
 
-  private void closeWriter(Writer writer) {
-    if (writer != null) {
-      try {
-        writer.close();
-      } catch (IOException e) {
-        getLogger().error("Fail to close the HFile writer", e);
-      }
-    }
-  }
+	private void closeWriter(Writer writer) {
+		if (writer != null) {
+			try {
+				writer.close();
+			} catch (IOException e) {
+				getLogger().error("Fail to close the HFile writer", e);
+			}
+		}
+	}
 
-  /**
-   * In the future, we can do a insert the donor id to integer id mapping to preserve the semantic of the search
-   */
-  @Override
-  public DataBag exec(Tuple input) throws IOException {
-    Configuration conf = UDFContext.getUDFContext().getJobConf();
-    String donorId = input.get(0).toString();
-    int id = Integer.valueOf(donorId);
-    if (writer == null) {
-      FileSystem fs = FileSystem.get(conf);
-      Path destPath = new Path(destDir, Bytes.toString(DATA_CONTENT_FAMILY));
-      if (!fs.exists(destPath)) fs.mkdirs(destPath);
+	/**
+	 * In the future, we can do a insert the donor id to integer id mapping to
+	 * preserve the semantic of the search
+	 */
+	@Override
+	public DataBag exec(Tuple input) throws IOException {
+		Configuration conf = UDFContext.getUDFContext().getJobConf();
+		String donorId = input.get(0).toString();
+		int id = Integer.valueOf(donorId);
+		if (writer == null) {
+			FileSystem fs = FileSystem.get(conf);
+			Path destPath = new Path(destDir,
+					Bytes.toString(DATA_CONTENT_FAMILY));
+			if (!fs.exists(destPath))
+				fs.mkdirs(destPath);
 
-      writer = createWriter(donorId, UDFContext.getUDFContext());
-      metaManager = new ArchiveMetaManager(metaTablename, conf);
-    } else if (id < previousId || isPerGroupOutput) {
-      // close previous writer first
-      closeWriter(writer);
-      writer = createWriter(donorId, UDFContext.getUDFContext());
-    }
+			writer = createWriter(donorId, UDFContext.getUDFContext());
+			metaManager = new ArchiveMetaManager(metaTablename, conf);
+		} else if (id < previousId || isPerGroupOutput) {
+			// close previous writer first
+			closeWriter(writer);
+			writer = createWriter(donorId, UDFContext.getUDFContext());
+		}
 
-    Properties props = UDFContext.getUDFContext().getUDFProperties(
-        this.getClass());
-    String headerline = props.getProperty("headerline");
-    Preconditions.checkNotNull(headerline);
-    headers = headerline.split(HEADER_SEPARATOR);
+		Properties props = UDFContext.getUDFContext().getUDFProperties(
+				this.getClass());
+		String headerline = props.getProperty("headerline");
+		Preconditions.checkNotNull(headerline);
+		headers = headerline.split(HEADER_SEPARATOR);
 
-    DataBag bag = (DataBag) input.get(1);
-    Iterator<Tuple> it = bag.iterator();
+		DataBag bag = (DataBag) input.get(1);
+		Iterator<Tuple> it = bag.iterator();
 
-    long n = 1;
-    DataBag output = BagFactory.getInstance().newDefaultBag();
+		long n = 1;
+		DataBag output = BagFactory.getInstance().newDefaultBag();
 
-    long totalBytes = 0;
-    while (it.hasNext()) {
-      Tuple t = it.next();
-      byte[] rowKey = SchemaUtil.encodedArchiveRowKey(id, n);
-      long now = System.currentTimeMillis();
-      for (byte i = 0; i < headers.length; ++i) {
-        Object cellValue = t.get(i);
-        if (cellValue == null) continue;
-        String value = (String) cellValue;
-        if (value.trim().isEmpty()) continue;
-        byte[] bytes = Bytes.toBytes(value);
-        KeyValue kv = new KeyValue(rowKey, DATA_CONTENT_FAMILY,
-            new byte[] { i }, now, bytes);
-        totalBytes = totalBytes + bytes.length;
-        writer.append(kv);
-      }
-      n++;
-      reporter.progress();
-    }
+		long totalBytes = 0;
+		while (it.hasNext()) {
+			Tuple t = it.next();
+			byte[] rowKey = SchemaUtil.encodedArchiveRowKey(id, n);
+			long now = System.currentTimeMillis();
+			for (byte i = 0; i < headers.length; ++i) {
+				Object cellValue = t.get(i);
+				if (cellValue == null)
+					continue;
+				String value = (String) cellValue;
+				if (value.trim().isEmpty())
+					continue;
+				byte[] bytes = Bytes.toBytes(value);
+				KeyValue kv = new KeyValue(rowKey, DATA_CONTENT_FAMILY,
+						new byte[] { i }, now, bytes);
+				totalBytes = totalBytes + bytes.length;
+				writer.append(kv);
+			}
+			n++;
+			reporter.progress();
+		}
 
-    if (n == 1) {
-      RuntimeException ex = new RuntimeException(
-          "No mutations for donor id: " + donorId);
-      getLogger().warn("No mutations for donor id: " + donorId, ex);
-      throw ex;
-    }
+		if (n == 1) {
+			RuntimeException ex = new RuntimeException(
+					"No mutations for donor id: " + donorId);
+			getLogger().warn("No mutations for donor id: " + donorId, ex);
+			throw ex;
+		}
 
-    previousId = id;
-    if (!hasDoneHeader) {
-      metaManager.keepMetaInfo(tablename, id, --n, totalBytes, headers);
-      hasDoneHeader = true;
-    } else {
-      metaManager.keepMetaInfo(tablename, id, --n, totalBytes);
-    }
+		previousId = id;
+		if (!hasDoneHeader) {
+			metaManager.keepMetaInfo(tablename, id, --n, totalBytes, headers);
+			hasDoneHeader = true;
+		} else {
+			metaManager.keepMetaInfo(tablename, id, --n, totalBytes);
+		}
 
-    Tuple result = TupleFactory.getInstance().newTuple(1);
-    result.set(0, new DataByteArray(SchemaUtil.encodedArchiveRowKey(id, n)));
-    output.add(result);
-    return output;
-  }
+		Tuple result = TupleFactory.getInstance().newTuple(1);
+		result.set(0, new DataByteArray(SchemaUtil.encodedArchiveRowKey(id, n)));
+		output.add(result);
+		return output;
+	}
 
-  @Override
-  public Schema outputSchema(Schema input) {
+	@Override
+	public Schema outputSchema(Schema input) {
 
-    UDFContext context = UDFContext.getUDFContext();
-    Properties udfProp = context.getUDFProperties(this.getClass());
+		UDFContext context = UDFContext.getUDFContext();
+		Properties udfProp = context.getUDFProperties(this.getClass());
 
-    List<FieldSchema> outputFields = newArrayListWithCapacity(50);
-    outputFields.add(new FieldSchema(null, DataType.BYTEARRAY));
-    StringBuilder sb = new StringBuilder();
-    try {
-      for (FieldSchema field : input.getField(1).schema.getField(0).schema
-          .getFields()) {
-        sb.append(field.alias);
-        sb.append(HEADER_SEPARATOR);
-      }
-      sb.setLength(sb.length() - HEADER_SEPARATOR.length());
-      String headerline = sb.toString();
-      udfProp.setProperty("headerline", headerline);
-    } catch (FrontendException e) {
-      throw new RuntimeException(e);
-    }
+		List<FieldSchema> outputFields = newArrayListWithCapacity(50);
+		outputFields.add(new FieldSchema(null, DataType.BYTEARRAY));
+		StringBuilder sb = new StringBuilder();
+		try {
+			for (FieldSchema field : input.getField(1).schema.getField(0).schema
+					.getFields()) {
+				sb.append(field.alias);
+				sb.append(HEADER_SEPARATOR);
+			}
+			sb.setLength(sb.length() - HEADER_SEPARATOR.length());
+			String headerline = sb.toString();
+			udfProp.setProperty("headerline", headerline);
+		} catch (FrontendException e) {
+			throw new RuntimeException(e);
+		}
 
-    // DataType.BAG
-    Schema recordSchema = new Schema(outputFields);
-    try {
-      return new Schema(new FieldSchema(null, recordSchema, DataType.BAG));
-    } catch (FrontendException e) {
-      throw new RuntimeException(e);
-    }
-  }
+		// DataType.BAG
+		Schema recordSchema = new Schema(outputFields);
+		try {
+			return new Schema(new FieldSchema(null, recordSchema, DataType.BAG));
+		} catch (FrontendException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-  @Override
-  public void finish() {
-    closeWriter(writer);
-    if (metaManager != null) {
-      metaManager.close();
-    }
-  }
+	@Override
+	public void finish() {
+		closeWriter(writer);
+		if (metaManager != null) {
+			metaManager.close();
+		}
+	}
 
 }
--- dcc-etl-exporter/src/main/java/org/icgc/dcc/etl/exporter/util/MaintenanceUtility.java
+++ dcc-etl-exporter/src/main/java/org/icgc/dcc/etl/exporter/util/MaintenanceUtility.java
@@ -28,7 +28,7 @@ import org.apache.hadoop.hbase.HRegionInfo;
 import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.ServerName;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
-import org.apache.hadoop.hbase.regionserver.compactions.CompactionRequest.CompactionState;
+import org.apache.hadoop.hbase.protobuf.generated.AdminProtos.GetRegionInfoResponse.CompactionState;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.icgc.dcc.downloader.core.SchemaUtil;
 
@@ -37,96 +37,107 @@ import com.google.common.collect.ImmutableList;
 @Slf4j
 public class MaintenanceUtility {
 
-  private static void runCompact(String releaseName, String dataType, boolean major) throws IOException,
-      InterruptedException {
-    HBaseAdmin admin = new HBaseAdmin(HBaseConfiguration.create());
-    try {
-      // for each table go through each region
-      HTableDescriptor[] tables = admin.listTables(SchemaUtil.getDataTableName(dataType, releaseName));
-      for (HTableDescriptor table : tables) {
-        log.info("-- compacting table: {}", table.getNameAsString());
+	private static void runCompact(String releaseName, String dataType,
+			boolean major) throws IOException, InterruptedException {
+		HBaseAdmin admin = new HBaseAdmin(HBaseConfiguration.create());
+		try {
+			// for each table go through each region
+			HTableDescriptor[] tables = admin.listTables(SchemaUtil
+					.getDataTableName(dataType, releaseName));
+			for (HTableDescriptor table : tables) {
+				log.info("-- compacting table: {}", table.getNameAsString());
 
-        try {
-          if (major) {
-            admin.majorCompact(table.getName());
-          } else {
-            admin.compact(table.getName());
-          }
-          int n = 0;
-          do {
-            wait(n);
-            n++;
-          } while (isCompactionDone(admin, table.getName()));
-        } catch (IOException e) {
-          log.warn("Table is not valid anymore: {}", table.getNameAsString(), e);
-        }
-      }
-    } finally {
-      admin.close();
-    }
-  }
+				try {
+					if (major) {
+						admin.majorCompact(table.getName());
+					} else {
+						admin.compact(table.getName());
+					}
+					int n = 0;
+					do {
+						wait(n);
+						n++;
+					} while (isCompactionDone(admin, table.getName()));
+				} catch (IOException e) {
+					log.warn("Table is not valid anymore: {}",
+							table.getNameAsString(), e);
+				}
+			}
+		} finally {
+			admin.close();
+		}
+	}
 
-  private static boolean isCompactionDone(HBaseAdmin admin, byte[] name) throws IOException,
-      InterruptedException {
-    return admin.getCompactionState(name) != CompactionState.NONE;
-  }
+	private static boolean isCompactionDone(HBaseAdmin admin, byte[] name)
+			throws IOException, InterruptedException {
+		return admin.getCompactionState(name) != CompactionState.NONE;
+	}
 
-  private static int[] TimeDelayInMilli =
-      new int[] { 15000, 1000, 1500, 1600, 1600, 1800, 2400, 3000, 4000, 5000, 8000, 10000, 14000, 20000 };
+	private static int[] TimeDelayInMilli = new int[] { 15000, 1000, 1500,
+			1600, 1600, 1800, 2400, 3000, 4000, 5000, 8000, 10000, 14000, 20000 };
 
-  private static void wait(int retries) throws InterruptedException {
-    if (retries < TimeDelayInMilli.length) {
-      TimeUnit.MILLISECONDS.sleep(TimeDelayInMilli[retries]);
-    } else {
-      TimeUnit.MILLISECONDS.sleep(20000);
-    }
-  }
+	private static void wait(int retries) throws InterruptedException {
+		if (retries < TimeDelayInMilli.length) {
+			TimeUnit.MILLISECONDS.sleep(TimeDelayInMilli[retries]);
+		} else {
+			TimeUnit.MILLISECONDS.sleep(20000);
+		}
+	}
 
-  public static void main(String[] argv) throws IOException, InterruptedException {
-    String releaseName = argv[0];
+	public static void main(String[] argv) throws IOException,
+			InterruptedException {
+		String releaseName = argv[0];
 
-    if (argv.length == 4 && Boolean.valueOf(argv[1])) {
-      balancer(releaseName);
-    }
+		if (argv.length == 4 && Boolean.valueOf(argv[1])) {
+			balancer(releaseName);
+		}
 
-    boolean major = false;
-    if (argv.length == 2) {
-      major = Boolean.valueOf(argv[1]);
-    }
-    String dataType = ".*.";
-    if (argv.length == 3) {
-      dataType = argv[1];
-    }
-    runCompact(releaseName, dataType, major);
-  }
+		boolean major = false;
+		if (argv.length == 2) {
+			major = Boolean.valueOf(argv[1]);
+		}
+		String dataType = ".*.";
+		if (argv.length == 3) {
+			dataType = argv[1];
+		}
+		runCompact(releaseName, dataType, major);
+	}
 
-  private static void balancer(String releaseName) throws IOException, InterruptedException {
-    HBaseAdmin admin = new HBaseAdmin(HBaseConfiguration.create());
-    try {
-      HTableDescriptor[] tables = admin.listTables(SchemaUtil.getDataTableName(".*.", releaseName));
-      for (HTableDescriptor table : tables) {
-        log.info("-- Balancing table: {}", table.getNameAsString());
+	private static void balancer(String releaseName) throws IOException,
+			InterruptedException {
+		HBaseAdmin admin = new HBaseAdmin(HBaseConfiguration.create());
+		try {
+			HTableDescriptor[] tables = admin.listTables(SchemaUtil
+					.getDataTableName(".*.", releaseName));
+			for (HTableDescriptor table : tables) {
+				log.info("-- Balancing table: {}", table.getNameAsString());
 
-        List<HRegionInfo> regionInfo = admin.getTableRegions(table.getName());
-        List<ServerName> servers = ImmutableList.copyOf(admin.getClusterStatus().getServers());
-        for (int i = 0; i < regionInfo.size(); ++i) {
-          HRegionInfo region = regionInfo.get(i);
-          if (!region.isOffline()) {
-            ServerName server = servers.get(i % servers.size());
-            String serverName = server.getServerName();
+				List<HRegionInfo> regionInfo = admin.getTableRegions(table
+						.getName());
+				List<ServerName> servers = ImmutableList.copyOf(admin
+						.getClusterStatus().getServers());
+				for (int i = 0; i < regionInfo.size(); ++i) {
+					HRegionInfo region = regionInfo.get(i);
+					if (!region.isOffline()) {
+						ServerName server = servers.get(i % servers.size());
+						String serverName = server.getServerName();
 
-            String regionEncodedName = regionInfo.get(i).getEncodedName();
-            log.info("Moving region: " + regionEncodedName + " to : " + serverName);
-            try {
-              admin.move(Bytes.toBytes(regionEncodedName), Bytes.toBytes(serverName));
-            } catch (IOException e) {
-              log.warn("fail to move region: {} ", regionEncodedName, e);
-            }
-          }
-        }
-      }
-    } finally {
-      admin.close();
-    }
-  }
+						String regionEncodedName = regionInfo.get(i)
+								.getEncodedName();
+						log.info("Moving region: " + regionEncodedName
+								+ " to : " + serverName);
+						try {
+							admin.move(Bytes.toBytes(regionEncodedName),
+									Bytes.toBytes(serverName));
+						} catch (IOException e) {
+							log.warn("fail to move region: {} ",
+									regionEncodedName, e);
+						}
+					}
+				}
+			}
+		} finally {
+			admin.close();
+		}
+	}
 }
