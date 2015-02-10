4a0fbc752bc8b051fe543a1173785cdcf5f1c4d9
Sepehr Mavedati <sepehr.mavedati@oicr.on.ca>
2015-02-10 16:40:02 -0500

    Updated scripts.

--- dcc-etl-exporter/src/main/bin/static-export.sh
+++ dcc-etl-exporter/src/main/bin/static-export.sh
@@ -6,7 +6,7 @@ source ${EXPORTHOMEDIR}/bin/setenv.sh
 start_time=`date +%s`
 
 # Export
-${EXPORTHOMEDIR}/bin/parallel -r -j ${#types[@]} "${EXPORTHOMEDIR}/pig/bin/pig -l ${EXPORTHOMEDIR}/logs/*.log -4 ${EXPORTHOMEDIR}/conf/log4j.properties ${EXPORTHOMEDIR}/scripts/exporter.py -s -d * -e ${EXPORTHOMEDIR}/scripts -i ${source} -r ${release}" ${types[@]}
+${EXPORTHOMEDIR}/bin/parallel -r -j ${#types[@]} "pig -l ${EXPORTHOMEDIR}/logs/*.log -4 ${EXPORTHOMEDIR}/conf/log4j.properties ${EXPORTHOMEDIR}/scripts/exporter.py -s -d * -e ${EXPORTHOMEDIR}/scripts -i ${source} -r ${release}" ${types[@]}
 
 end_time=`date +%s`
 echo Total export time was `expr $end_time - $start_time` s.
