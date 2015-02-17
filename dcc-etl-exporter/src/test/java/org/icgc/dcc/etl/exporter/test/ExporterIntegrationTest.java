f0b432358c70d230032c69771084c76c26be70b6
Bob Tiernay <btiernay@oicr.on.ca>
2015-02-16 20:43:23 -0500

    Fixed logging exclusions for testing dependencies. Added EmbeddedHBase. Add ExporterIntegrationTest shell.

---
+++ dcc-etl-exporter/src/test/java/org/icgc/dcc/etl/exporter/test/ExporterIntegrationTest.java
@@ -0,0 +1,70 @@
+package org.icgc.dcc.etl.exporter.test;
+
+import java.io.File;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+import org.icgc.dcc.etl.exporter.test.hbase.EmbeddedHBase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+@Slf4j
+@Ignore("Nothing implemented yet. See DCC-2944 for details")
+public class ExporterIntegrationTest {
+
+  /**
+   * Constants.
+   */
+   static final String ZK_CONNECT = "localhost:21812";
+   
+   /**
+    * Configuration.
+    */
+   File loaderDir;
+  
+  /**
+   * Collaborators.
+   */
+  EmbeddedHBase hbase;
+
+  /**
+   * State.
+   */
+  @Rule
+  public TemporaryFolder tmp = new TemporaryFolder();
+
+  @Before
+  @SneakyThrows
+  public void setUp() {
+    this.hbase = new EmbeddedHBase(ZK_CONNECT);
+    
+    log.info("> Starting embedded HBase...\n");
+    hbase.startUp();
+    log.info("< Started embedded HBase");
+    
+    log.info("> Creating loader directory...\n");
+    this.loaderDir = tmp.newFolder();
+    log.info("< Created loader directory: {}", loaderDir);
+  }
+
+  @SneakyThrows
+  @After
+  public void shutDown() {
+    log.info("> Stopping embedded HBase...");
+    hbase.shutDown();
+    hbase = null;
+    log.info("< Stopped embedded HBase");
+  }
+  
+  @Test
+  public void testExporter() {
+    // TODO: Implement
+    log.info("Testing...");
+  }
+
+}
