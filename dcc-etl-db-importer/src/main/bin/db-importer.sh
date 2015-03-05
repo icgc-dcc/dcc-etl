0c36189ed997f6125c409092abd7d680ab03cfad
Sepehr Mavedati <sepehr.mavedati@oicr.on.ca>
2015-03-05 16:03:42 -0500

    Fixed several issues with db importer.

--- dcc-etl-db-importer/src/main/bin/db-importer.sh
+++ dcc-etl-db-importer/src/main/bin/db-importer.sh
@@ -6,17 +6,15 @@
 #   Runs db-importer
 #
 # Usage:
-#   ./db-importer.sh --config /path/to/config/file [--collections projects,genes,cgc,go,pathways]
+#   ./db-importer.sh [--collections PROJECTS, CGC, GO, PATHWAYS, GENES]
 #
 # Example:
-#   ./db-importer.sh --config ***REMOVED***/dcc-etl/conf/etl_dev.yaml --collection projects,genes
+#   ./db-importer.sh --collection PROJECTS, GENES
 
 base_dir=$(dirname $0)/..
 java_opts="-Xmx4g -Xms4g"
 
 java \
   ${java_opts} \
--jar ${base_dir}/lib/dcc-etl-db-importer.jar "$@" \
--Dlogback.configurationFile=${base_dir}/conf/logback.xml \
---config=${base_dir}/conf/config.yaml \
---collections=projects,genes,cgc,go,pathways
+-jar ${base_dir}/lib/dcc-etl-db-importer.jar --config ${base_dir}/conf/config.yaml "$@" \
+-Dlogback.configurationFile=${base_dir}/conf/logback.xml
