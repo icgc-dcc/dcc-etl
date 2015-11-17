2b51aac4dc143283f34bea3539710a3b9e4d1b89
Vitalii Slobodianyk <vitalii.slobodianyk@oicr.on.ca>
2015-11-17 12:24:15 -0500

    Fixed https://jira.oicr.on.ca/browse/DCC-2500

--- dcc-etl-indexer/src/main/java/org/icgc/dcc/etl/indexer/util/MutationCentricFeatureConverter.java
+++ dcc-etl-indexer/src/main/java/org/icgc/dcc/etl/indexer/util/MutationCentricFeatureConverter.java
@@ -135,7 +135,7 @@ public class MutationCentricFeatureConverter {
   }
 
   private static String text(JsonNode jsonNode) {
-    return nullToEmpty(jsonNode.textValue());
+    return nullToEmpty(jsonNode.asText());
   }
 
 }
